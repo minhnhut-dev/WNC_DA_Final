88174f2994d5a6a65ae28271d6bedb58245669fa
Minh Nhut dev <nhutpham@advn.vn>
2023-12-22 01:10:25 +0700

    change load places

--- src/Components/map/index.jsx
+++ src/Components/map/index.jsx
@@ -5,13 +5,14 @@ import "@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css";
 import "mapbox-gl/dist/mapbox-gl.css";
 import "./map.css";
 import { geoJson } from "../../utils/geojson";
+import {MAPBOX_ACCESS_TOKEN} from "../../config/config";
 
 const Map = () => {
-  mapboxgl.accessToken =
-    "pk.eyJ1Ijoibmh1dHBoYW1kZXYiLCJhIjoiY2xvbGcwZm5sMG1lMDJpbnJuemNmYm1xYyJ9.w9hEet44dcjmTnu7LWUkWQ";
+  mapboxgl.accessToken = MAPBOX_ACCESS_TOKEN;
   const mapContainerRef = useRef(null);
-function createElementReport(address, full_address) {
-  let reportElement = `<div class="container">
+
+  function createElementReport(address, full_address) {
+    let reportElement = `<div class="container">
   <div class="row">
       <div class="col-12">
           <div class="info-box">
@@ -36,130 +37,143 @@ function createElementReport(address, full_address) {
       </div>
   </div>
 </div>`;
- 
-return reportElement;
-}
 
-async function reverseGeocoding(lat, long) {
-  try {
-    const response = await fetch(
-      `https://api.mapbox.com/geocoding/v5/mapbox.places/${long},${lat}.json?access_token=pk.eyJ1Ijoibmh1dHBoYW1kZXYiLCJhIjoiY2xvbGcwZm5sMG1lMDJpbnJuemNmYm1xYyJ9.w9hEet44dcjmTnu7LWUkWQ`
-    );
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.log(error);
+    return reportElement;
   }
-}
+
+  async function reverseGeocoding(lat, long) {
+    try {
+      const response = await fetch(
+        `https://api.mapbox.com/geocoding/v5/mapbox.places/${long},${lat}.json?access_token=pk.eyJ1Ijoibmh1dHBoYW1kZXYiLCJhIjoiY2xvbGcwZm5sMG1lMDJpbnJuemNmYm1xYyJ9.w9hEet44dcjmTnu7LWUkWQ`
+      );
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
   useEffect(() => {
     const map = new mapboxgl.Map({
       container: mapContainerRef.current,
-      style: "mapbox://styles/mapbox/streets-v11",
-      center: [106.707222, 10.752444], // Ho Chi Minh City
-      zoom: 12,
-    });
-    map.on("load", () => {
-      map.addSource("points", {
-        type: "geojson",
-        data: geoJson,
-        cluster: true, // Enables clustering
-        clusterMaxZoom: 14, // Maximum zoom level at which clustering is enabled
-        clusterRadius: 50, // The radius of each cluster when clustering points
-      });
+      style: "mapbox://styles/mapbox/streets-v12",
+      center: [ 106.7008, 10.7769],
+      maxZoom: 20,
+      zoom: 14,
 
-      map.addLayer({
-        id: 'clusters',
-        type: 'circle',
-        source: 'points',
-        filter: ['has', 'point_count'],
-        paint: {
-          'circle-color': [
-            'step',
-            ['get', 'point_count'],
-            '#51bbd6',
-            100,
-            '#f1f075',
-            750,
-            '#f28cb1'
-          ],
-          'circle-radius': [
-            'step',
-            ['get', 'point_count'],
-            20,
-            100,
-            30,
-            750,
-            40
-          ]
-        }
-      });
-
-      map.addLayer({
-        id: "unclustered-point",
-        type: "circle",
-        source: "points",
-        filter: ["!", ["has", "point_count"]], // Filter out clustered points
-        paint: {
-          "circle-color": "#11b4da",
-          "circle-radius": 15,
-          "circle-stroke-width": 1,
-          "circle-stroke-color": "#fff",
-        },
-      });
-      map.addLayer({
-        id: 'unclustered-point-label',
-        type: 'symbol',
-        source: 'points',
-        filter: ['!', ['has', 'point_count']], // Use the same filter as your circle layer
-        layout: {
-            'text-field': 'QC', // This is the text that will be displayed
-            'text-font': ['Open Sans Semibold', 'Arial Unicode MS Bold'], // Set the text font
-            'text-size': 12 // Set the text size
-        },
-        paint: {
-            'text-color': '#ffffff' // Set the text color
-        }
-      });
     });
 
-    map.addControl(
-      new mapboxgl.GeolocateControl({
-        positionOptions: {
-          enableHighAccuracy: true,
-        },
-        trackUserLocation: true,
-      }),
-      "bottom-right"
-    );
-
-    const geocoder = new MapboxGeocoder({
-      accessToken: mapboxgl.accessToken, // Set the access token
-      mapboxgl: mapboxgl, // Set the mapbox-gl instance
-      marker: true, // Use the geocoder's default marker style
-      bbox: [106.6297, 10.6958, 106.8413, 10.8765], // Set the bounding box coordinates
-      placeholder: "Tìm kiếm địa điểm", // Placeholder text for the search bar,
-      autocomplete: true,
-    });
     let lng;
     let lat;
-    map.addControl(geocoder, "top-left");
 
     map.on("click", async (event) => {
       lng = event.lngLat.lng.toFixed(6);
       lat = event.lngLat.lat.toFixed(6);
       const data = await reverseGeocoding(lat, lng);
       let features = data.features;
-       let Address = features[0].text; // địa danh: "Bách hoá xanh, chợ, trường, công ty,..."
+      let Address = features[0].text; // địa danh: "Bách hoá xanh, chợ, trường, công ty,..."
       let  ward = features[1].text;
       let district = features[3].text;
       let city = features[4].text;
       const fullAddress = `${Address}, ${ward}, ${district}, ${city}`;
       const elementReport = createElementReport(Address, fullAddress);
-    
+
       new mapboxgl.Popup()
         .setLngLat(event.lngLat)
         .setHTML(elementReport)
         .addTo(map);
+    });
+
+    map.on('load', () => {
+      map.addSource('places', {
+        'type': 'geojson',
+        'data': geoJson
+      });
+
+
+      map.addLayer({
+        id: 'myDataCircles',
+        type: 'circle',
+        source: 'places',
+        paint: {
+          'circle-radius': 15,
+          "circle-color": "#11b4da",
+          "circle-stroke-width": 1,
+          "circle-stroke-color": "#fff",
+        }
+      });
+      
+      // Add a new layer to the map for the labels
+      map.addLayer({
+        id: 'myDataLabels',
+        type: 'symbol',
+        source: 'places',
+        layout: {
+          'text-field': 'QC',
+          'text-size': 9
+        }
+      });
+
+      map.addControl(
+        new mapboxgl.GeolocateControl({
+          positionOptions: {
+            enableHighAccuracy: true,
+          },
+          trackUserLocation: true,
+        }),
+        "bottom-right"
+      );
+  
+      const geocoder = new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken, // Set the access token
+        mapboxgl: mapboxgl, // Set the mapbox-gl instance
+        marker: true, // Use the geocoder's default marker style
+        bbox: [106.6297, 10.6958, 106.8413, 10.8765], // Set the bounding box coordinates
+        placeholder: "Tìm kiếm địa điểm", // Placeholder text for the search bar,
+        autocomplete: true,
+      });
+      
+      map.addControl(geocoder, "top-left");
+      const marker = new mapboxgl.Marker({
+        color: '#314ccd'
+      });
+
+      map.on('click', 'myDataCircles', (e) => {
+        const coordinates = e.features[0].geometry.coordinates.slice();
+        const description = e.features[0].properties.description;
+
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+          coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+        }
+        new mapboxgl.Popup()
+          .setLngLat(coordinates)
+          .setHTML(description)
+          .addTo(map);
+      });
+
+      // Change the cursor to a pointer when the mouse is over the places layer.
+      map.on('mouseenter', 'myDataCircles', function() {
+        map.getCanvas().style.cursor = 'pointer';
+      });
+      
+      // When the mouse leaves a feature in the 'placesCircles' layer, change the cursor style back to default
+      map.on('mouseleave', 'myDataCircles', function() {
+        map.getCanvas().style.cursor = '';
+      });
+
+    
+
+      map.on('click' , (event) => {
+        console.log(event);
+        marker.setLngLat(event.lngLat).addTo(map);
+
+        if(map.getLayer('places')) {
+         return;
+        }else {
+          alert('Bạn chưa chọn địa điểm');
+        }
+      });
+
     });
     // Clean up on unmount
     return () => map.remove();
@@ -167,7 +181,7 @@ async function reverseGeocoding(lat, long) {
 
   return (
     <>
-      <div id="map" ref={mapContainerRef}  />;
+      <div id="map" ref={mapContainerRef} />;
     </>
   );
 };
--- src/utils/geojson.js
+++ src/utils/geojson.js
@@ -1,118 +1,44 @@
  export const geoJson = {
-  type: "FeatureCollection",
-  features: [
+  'type': 'FeatureCollection',
+  'features': [
     {
-      type: "Feature",
-      geometry: {
-        type: "Point",
-        coordinates: [106.682462, 10.764324],
+      'type': 'Feature',
+      'properties': {
+        'description':
+          '<strong>Make it Mount Pleasant</strong><p><a href="http://www.mtpleasantdc.com/makeitmtpleasant" target="_blank" title="Opens in a new window">Make it Mount Pleasant</a> is a handmade and vintage market and afternoon of live entertainment and kids activities. 12:00-6:00 p.m.</p>',
+        'text': 'theatre'
       },
-      properties: {
-        name: "Now zone",
-      },
+      'geometry': {
+        'type': 'Point',
+        'coordinates': [106.733919, 10.725548]
+      }
     },
     {
-      type: "Feature",
-      geometry: {
-        type: "Point",
-        coordinates: [106.682806, 10.763993],
+      'type': 'Feature',
+      'properties': {
+        'description':
+          '<strong>Mad Men Season Five Finale Watch Party</strong><p>Head to Lounge 201 (201 Massachusetts Avenue NE) Sunday for a <a href="http://madmens5finale.eventbrite.com/" target="_blank" title="Opens in a new window">Mad Men Season Five Finale Watch Party</a>, complete with 60s costume contest, Mad Men trivia, and retro food and drink. 8:00-11:00 p.m. $10 general admission, $20 admission and two hour open bar.</p>',
+        'text': 'theatre'
       },
-      properties: {
-        name: "PNJ",
-      },
+      'geometry': {
+        'type': 'Point',
+        'coordinates': [106.733879, 10.725734]
+      }
     },
     {
-      type: "Feature",
-      geometry: {
-        type: "Point",
-        coordinates: [106.682475, 10.762828],
+      'type': 'Feature',
+      'properties': {
+        'description':
+          '<strong>Big Backyard Beach Bash and Wine Fest</strong><p>EatBar (2761 Washington Boulevard Arlington VA) is throwing a <a href="http://tallulaeatbar.ticketleap.com/2012beachblanket/" target="_blank" title="Opens in a new window">Big Backyard Beach Bash and Wine Fest</a> on Saturday, serving up conch fritters, fish tacos and crab sliders, and Red Apron hot dogs. 12:00-3:00 p.m. $25.grill hot dogs.</p>',
+        'text': 'bar'
       },
-      properties: {
-        name: "Trường đại học khoa học tự nhiên",
-      },
+      'geometry': {
+        'type': 'Point',
+        'coordinates': [106.734006, 10.726029]
+      }
     },
-    {
-      type: "Feature",
-      geometry: {
-        type: "Point",
-        coordinates: [106.681473, 10.763485],
-      },
-      properties: {
-        name: "Trường trung học phổ thông chuyên Lê Hồng Phong",
-      },
-    },
-    {
-      type: "Feature",
-      geometry: {
-        type: "Point",
-        coordinates: [106.682756, 10.762039],
-      },
-      properties: {
-        name: "Circle K Nguyễn Văn Cừ",
-      },
-    },
-    {
-      type: "Feature",
-      geometry: {
-        type: "Point",
-        coordinates: [106.682895, 10.764272],
-      },
-      properties: {
-        name: "Khách sạn Nikko",
-      },
-    },
-    {
-      type: "Feature",
-      geometry: {
-        type: "Point",
-        coordinates: [106.682121,10.763259],
-      },
-      properties: {
-        name: "Nhà xuất bản giáo dục",
-      },
-    },
-    {
-      type: "Feature",
-      geometry: {
-        type: "Point",
-        coordinates: [106.685082,10.764387],
-      },
-      properties: {
-        name: "Khách sạn litle",
-      },
-    },
-    {
-      type: "Feature",
-      geometry: {
-        type: "Point",
-        coordinates: [106.733903,10.725540],
-      },
-      properties: {
-        name: "Nhà Thuốc",
-      },
-    },
-    {
-      type: "Feature",
-      geometry: {
-        type: "Point",
-        coordinates: [106.682478,10.764171],
-      },
-      properties: {
-        name: "Pizza hut",
-      },
-    },
-    {
-      type: "Feature",
-      geometry: {
-        type: "Point",
-        coordinates: [106.625551,10.727925],
-      },
-      properties: {
-        name: "BW Building",
-      },
-    }
-  ],
-};
+  ]
+}
 
 
 
